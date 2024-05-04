e13933c756f1bc92c105e22e0b1cb82a3b4e0af9
BritoAlv <gomzalezbrito@gmail.com>
2024-05-04 11:34:55 -0400

    update to remove repl by now

---
+++ main.py
@@ -0,0 +1,41 @@
+import argparse
+from pathlib import Path
+from lexer.lexer import *
+from visitors.visitor import *
+from parser.parser import *
+
+def solve(inputStr):
+    l = Lexer(inputStr)
+    l.scanTokens()
+    parser = Parser(l.tokens)
+    program = parser.parseProgram()
+    printer = TreePrinter() 
+    evaluator = AstEvaluator()
+    for statment in program:
+        print(statment.accept(printer))
+        statment.accept(evaluator)
+
+def main():
+    # Create the parser
+    parser = argparse.ArgumentParser(description='Process a file path.')
+    
+    # Add the arguments
+    parser.add_argument('file_path', type=Path, help='Path to the file')
+    
+    # Parse the arguments
+    args = parser.parse_args()
+    
+    # Check if the file exists
+    if args.file_path.exists():
+        try:
+            with open(args.file_path, "r") as f:
+                cont = f.read()
+                solve(cont)
+                f.close()
+        except Exception as e:
+            print(e)    
+    else:
+        print(f'File does not exist: {args.file_path}')
+
+if __name__ == '__main__':
+    main() 
--- makefile
+++ makefile
@@ -1,9 +1,2 @@
 make run:
-	python3 repl.py
-
-make test:
-	g++ expression-generator.cpp -o expgen.out
-	echo > cases.txt $(CASES)
-	./expgen.out < cases.txt > expgen.txt   
-	python3 tester.py < expgen.txt > result.txt
-	cat result.txt
+	python3 main.py "./program.txt"
