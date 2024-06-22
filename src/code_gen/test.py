a82b6ac036a2a3ebde0313125f8acdc1be7705ad
Javier Lima <javi.limashelby@gmail.com>
2024-06-22 10:23:37 -0400

    test: Test code-gen with simple while

--- src/code_gen/test.py
+++ src/code_gen/test.py
@@ -1,8 +1,7 @@
-from typing import Literal
 from code_gen.environment_builder import EnvironmentBuilder
 from code_gen.generator import Generator
 from code_gen.resolver import Resolver
-from common.ast_nodes.expressions import BinaryNode, CallNode, IfNode, LetNode, LiteralNode
+from common.ast_nodes.expressions import BinaryNode, CallNode, DestructorNode, IfNode, LetNode, LiteralNode, WhileNode
 from common.ast_nodes.statements import MethodNode, ProgramNode
 from common.token_class import Token
 
@@ -79,6 +78,28 @@ ast = ProgramNode(
                CallNode(LiteralNode(Token('id', 'fibonacci')), [
                    LiteralNode(Token('number', '6'))
                ]), 
+               Token('id', 'number'))]
+)
+
+ast = ProgramNode(
+    [MethodNode(Token('id', 'loop'), [
+        (Token('id', 'n'), None)
+    ],
+        WhileNode(
+                   BinaryNode(LiteralNode(Token('id', 'n')), 
+                              Token('less', '<'),
+                              LiteralNode(Token('number', '10'))),
+                   DestructorNode(Token('id', 'n'), BinaryNode(
+                       LiteralNode(Token('id', 'n')),
+                       Token('plus', '+'),
+                       LiteralNode(Token('number', '1'))
+                   ))
+               ),
+               Token('id', 'number')),
+      MethodNode(Token('id', 'main'), [], 
+               CallNode(LiteralNode(Token('id', 'loop')), [
+                   LiteralNode(Token('number', '1'))
+               ]),
                Token('id', 'number'))]
 )
 
