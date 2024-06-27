9e191a7ff9a4826ccd2241d9ea755bb389cb280e
BritoAlv <gomzalezbrito@gmail.com>
2024-06-27 18:32:36 -0400

    fix main

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import argparse
+from os import mkdir
 import sys
 
 from code_gen.constructor_builder import ConstructorBuilder
 from code_gen.environment_builder import EnvironmentBuilder
 from code_gen.generator import Generator
 from code_gen.resolver import Resolver
+from common.ast_nodes.statements import ProgramNode
+from common.parse_nodes.parse_tree import ParseTree
 from common.printer import TreePrinter
+from common.token_class import Token
 from lexing.lexer.main import *
 from parsing.parser.parser import Parser
 from semantic.tipos import SemanticAnalysis
+import re
+import pexpect
 
 # Set up argument parsing
 parser = argparse.ArgumentParser(description='Hulk Compiler')
@@ -20,18 +26,13 @@ parser.add_argument('-cg', '--codegen', action='store_true', help='Generate code
 parser.add_argument('-r', '--run', action='store_true', help='Run the compiled assembly')
 
 defaultHulkProgram = """
-        type Perro(color : string, color: number)
-        {
-            color = color;
-            edad = edad;
-            Ladrar(a: number, af: string) : number => print("Wolf" @ "Wolf");
-        }
-        let a = new Perro("Negro", 7), c = new Perro("Negro", 7), b = [1, "3"] in b[if(2 == ) 0 else 1];
-        """
+    print(a);
+"""
 
 inputStr = defaultHulkProgram
 
-def codeGen(inputStr : str):
+
+def lex(inputStr : str, show = False) -> list[Token]:
     tokens = hulk_lexer.scanTokens(inputStr)
     if hulk_lexer.report(inputStr) :
         sys.exit(1)
@@ -46,6 +47,15 @@ def parse(inputStr : str, show = False) -> ParseTree:
     tokens = lex(inputStr)
     parser = Parser()
     parse_tree = parser.parse(tokens)
+    if show:
+        print("Parse Tree:")
+        parse_tree.root.print([0], 0, True)
+        print("\n")
+    return parse_tree
+
+def ast(inputStr : str, show = False) -> ProgramNode:
+    parse_tree = parse(inputStr)
+    parser = Parser()
     ast = parser.toAst(parse_tree)
     if show:
         print("AST:")
@@ -56,7 +66,7 @@ def parse(inputStr : str, show = False) -> ParseTree:
 def semantic_analysis(inputStr : str) -> ProgramNode:
     treeAst = ast(inputStr)
     sem_an = SemanticAnalysis()
-    #sem_an.run(treeAst)
+    sem_an.run(treeAst)
     return treeAst
 
 def codeGen(inputStr : str, show = False) -> str:
@@ -67,76 +77,98 @@ def codeGen(inputStr : str, show = False) -> str:
     environment = environment_builder.build(ast)
     resolver = Resolver(environment)
     generator = Generator(resolver)
-    print("Generated Code:")
-    print(generator.generate(ast))
-    print("\n")
-
-def lex(inputStr : str):
-    tokens = hulk_lexer.scanTokens(inputStr)
-    print("Tokens:")
-    for token in tokens:
-        print(token)
-    print("\n")
-
-def parse(inputStr : str):
-    tokens = hulk_lexer.scanTokens(inputStr)
-    parser = Parser()
-    parse_tree = parser.parse(tokens)
-    print("Parse Tree:")
-    parse_tree.root.print([0], 0, True)
-    print("\n")
-
-def ast(inputStr : str):
-    tokens = hulk_lexer.scanTokens(inputStr)
-    parser = Parser()
-    parse_tree = parser.parse(tokens)
-    ast = parser.toAst(parse_tree)
-    print("AST:")
-    print(ast.accept(TreePrinter()))
-    print("\n")
-
-def semantic_analysis(inputStr : str):
-    tokens = hulk_lexer.scanTokens(inputStr)
-    parser = Parser()
-    parse_tree = parser.parse(tokens)
-    ast = parser.toAst(parse_tree)
+    if show:
+        print("Generated Code:")
+        print(generator.generate(ast))
+        print("\n")
+    return generator.generate(ast)
     
-    sem_an = SemanticAnalysis()
-    sem_an.run(ast)
-
 def run(inputStr : str):
-    # run the assembly code somehow
-    pass
+    assembly = codeGen(inputStr)
+
+    # Paths of assembly files
+    file1_name = '.bin/main.asm'
+    file2_name = '.bin/std.asm'
+    file3_name = '.bin/stack.asm'
+
+    try:
+        mkdir('.bin/')
+    except:
+        pass
+
+    with open('src/code_gen/assembly/std.asm', 'r') as source:
+        with open(file2_name, 'w') as target:
+            target.write(source.read())
+
+    with open('src/code_gen/assembly/stack.asm', 'r') as source:
+        with open(file3_name, 'w') as target:
+            target.write(source.read())
+
+    with open(file1_name, 'w') as file:
+        file.write(assembly)
+
+    startup_regex = re.compile(
+        r'SPIM Version \d+\.\d+ of \w+ \d+, \d+\r\n'
+        r'Copyright \d+-\d+, James R\. Larus\.\r\n'
+        r'All Rights Reserved\.\r\n'
+        r'See the file README for a full copyright notice\.\r\n'
+        r'Loaded: /usr/lib/spim/exceptions\.s\r\n'
+        r'\(spim\) '
+    )
+
+    try:
+        # Start the SPIM process
+        spim = pexpect.spawn('spim')
+
+        # Wait for the SPIM prompt
+        spim.expect(startup_regex.pattern)
+
+        # Load the first file
+        spim.sendline(f'load "{file1_name}"')
+        spim.expect_exact('(spim) ')
+
+        # Load the second file
+        spim.sendline(f'load "{file2_name}"')
+        spim.expect_exact('(spim) ')
+
+        # Load the third file
+        spim.sendline(f'load "{file3_name}"')
+        spim.expect_exact('(spim) ')
+
+        spim.sendline(f'run')
+        spim.sendline(f'ex')
+        spim.interact()
+        
+    except pexpect.exceptions.ExceptionPexpect as e:
+        print(f"Error running SPIM: {e}")
 
 if len(sys.argv) == 1:
-    ast(inputStr)
-    print(inputStr)
-    semantic_analysis(inputStr)
+    run(inputStr)
     sys.exit(0)
-    
-# Parse arguments
-args = parser.parse_args()
+else:    
+    # Parse arguments
+    args = parser.parse_args()
 
-# Use the input argument
-inputStr = sys.stdin.read().strip()
+    # Use the input argument
+    # inputStr = sys.stdin.read().strip()
 
-if inputStr == None or inputStr == "":
-    inputStr = defaultHulkProgram
+    if inputStr == None or inputStr == "":
+        inputStr = defaultHulkProgram
 
-if args.lex:
-    lex(inputStr)
+    if args.lex:
+        lex(inputStr, True)
 
-if args.parse:
-    parse(inputStr)
+    if args.parse:
+        parse(inputStr, True)
 
-if args.ast:
-    ast(inputStr)
+    if args.ast:
+        ast(inputStr, True)
 
-if args.semantic_analysis:
-    semanticAnalysis(inputStr)
+    if args.semantic_analysis:
+        semantic_analysis(inputStr)
 
-if args.codegen:
-    codeGen(inputStr)
+    if args.codegen:
+        codeGen(inputStr, True)
 
     if args.run:
         run(inputStr)
